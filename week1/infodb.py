4ccd77b32d4e14b2ae3922ac0f106b73b60110ce
Jun <87632049+peacekeeper6@users.noreply.github.com>
2022-03-17 21:16:04 +0000

    infodb

---
+++ week1/infodb.py
@@ -0,0 +1,21 @@
+# Hack 1: InfoDB lists.  Build your own/personalized InfoDb with a list length > 3,  create list within a list as illustrated with Owns_Cars
+
+InfoDb = []
+# List with dictionary records placed in a list  
+InfoDb.append({  
+               "FirstName": "Jun",  
+               "LastName": "Lim",
+               "DOB": "March 3",  
+               "Residence": "San Diego",  
+               "Email": "junlim954@gmail.com",  
+               "Tri3_Teachers":["Smith","Buehler","Cheskaty","Dafoe", "Craig", "Naidu"]  
+              })  
+
+InfoDb.append({  
+               "FirstName": "Lorem",  
+               "LastName": "Ipsum",  
+               "DOB": "January 1",  
+               "Residence": "World",  
+               "Email": "loremipsum@gmail.com",  
+               "Tri3_Teachers"":["is","simply","dummy","text", "of", "the", "printing", "and", "typesetting", "industry"]  
+              })  
